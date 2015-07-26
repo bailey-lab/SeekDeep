463cdce00ee1408a8a023974f4442005db06a0c9
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-07-26 14:30:09 -0400

    update for njhTable to use a dropdown menu instead of a scatter of checkbox to select certain columns

--- serverResources/js/own/njhCheckboxMenu.js
+++ serverResources/js/own/njhCheckboxMenu.js
@@ -1,3 +1,8 @@
+// Allow Bootstrap dropdown menus to have forms/checkboxes inside, 
+// and when clicking on a dropdown item, the menu doesn't disappear.
+$(document).on('click', '.dropdown-menu.dropdown-menu-form', function(e) {
+  e.stopPropagation();
+});
 
 function njhCheckboxMenu(divSelector, names, updateFunction){
 	this.divSelector = divSelector;
@@ -31,6 +36,114 @@ function njhCheckboxMenu(divSelector, names, updateFunction){
 
 	//add the un-check all and check all functions
 	var self = this;
+	menu.select(".njhCheckAll").on("click", function(){
+		menu.selectAll("input").property("checked", true);
+		self.updateFunc();
+	});
+	menu.select(".njhUncheckAll").on("click", function(){
+		menu.selectAll("input").property("checked", false);
+		self.updateFunc();
+	});
+	//add update function to the checkboxes
+	menu.selectAll("input").on("click", this.updateFunc);
+};
+
+
+function njhCheckboxMenuOrganized(divSelector, names, updateFunction){
+	this.divSelector = divSelector;
+	this.names = names;
+	this.updateFunc = updateFunction;
+	var self = this;
+	var menuCats = {"misc": []}
+	for (obj in names){
+		
+		if(names[obj].indexOf("_") > -1){
+			var pre = names[obj].substr(0, names[obj].indexOf("_") + 1);
+			if(pre in menuCats){
+				menuCats[pre].push(names[obj]);
+			}else{
+				menuCats[pre] = [names[obj]];
+			}
+		}else if (names[obj].indexOf(".") > -1){
+			var pre = names[obj].substr(0, names[obj].indexOf(".") + 1);
+			if(pre in menuCats){
+				menuCats[pre].push(names[obj]);
+			}else{
+				menuCats[pre] = [names[obj]];
+			}
+		}else{
+			menuCats["misc"].push(names[obj]);
+		}
+	}
+	var menuCatsData = [];
+	keys = Object.keys(menuCats),
+	keys.sort();
+	for (i = 0; i < keys.length; i++){
+		cat = keys[i];
+		var currentCat = {name : cat, subNames : menuCats[cat]};
+		menuCatsData.push(currentCat);
+	}
+	//console.log(menuCatsData);
+	//console.log(menuCats);
+	var menu = d3.select(this.divSelector);
+	var nav = menu.append("ul").attr("class", "nav nav-pills");
+	var cats = menu.select("ul").selectAll("li")
+		.data(menuCatsData)
+		.enter().append("li")
+			.attr("class", "dropdown active")
+		
+			
+	cats.append("a")
+		.attr("class", "dropdown-toggle")
+		.attr("data-toggle", "dropdown")
+		.attr("href", "#")
+		.html(function(d){ return d.name + "<span class=\"caret\"></span>"});
+	var labs = cats.append("ul")
+		.attr("class", "dropdown-menu dropdown-menu-form")
+		.attr("id", function(d){ return "menu_" + d.name.replace(".", "");})
+		.selectAll("li")
+		.data(function(d){ return d.subNames;})
+		.enter().append("li")
+			.append("div")
+				.attr("class", "checkbox")
+				.attr("style", "margin-left: 2px")
+			.append("label");
+	labs.html(function(d){return "<input type=\"checkbox\" name=\"check\" checked=\"true\" value=" + d + " id=" + d + ">" + d;})
+		;//.attr("style", "border:2px solid black; padding: 2px;margin: 1px");
+	/*labs.append("input")
+		.attr("type","checkbox")
+		.attr("name", "check")
+		.attr("checked", true)
+		.attr("value",function(d){return d;})
+		.attr("id", function(d){return d;});*/
+
+		
+	var drops = cats.select("ul");
+	drops.append("li").append("a")
+		.attr("href", "javascript:void(0);")
+		.text("Check All").on("click", function(d){
+			cats.select("#menu_" + d.name.replace(".", "")).selectAll("input").property("checked", true);
+			self.updateFunc();
+		});
+	drops.append("li").append("a")
+		.attr("href", "javascript:void(0);")
+		.text("Uncheck All").on("click", function(d){
+			cats.select("#menu_" + d.name.replace(".", "")).selectAll("input").property("checked", false);
+			self.updateFunc();
+		});
+	//Add check all and uncheck all buttons on their own lines
+	menu.append("a")
+		.attr("href", "javascript:void(0);")
+		.attr("class", "njhCheckAll")
+		.text("Check All");
+	menu.append("br");
+	menu.append("a")
+		.attr("href", "javascript:void(0);")
+		.attr("class", "njhUncheckAll")
+		.text("Uncheck All");
+
+	//add the un-check all and check all functions
+
 	menu.select(".njhCheckAll").on("click", function(){
 		menu.selectAll("input").property("checked", true);
 		self.updateFunc();
--- serverResources/js/own/njhTable.js
+++ serverResources/js/own/njhTable.js
@@ -5,17 +5,46 @@ function njhTable(masterDivId, tableMasterData, tableDownloadStubName, addChart)
 	this.tableMasterData = tableMasterData;
 	this.tableDownloadStubName = tableDownloadStubName;
 	//create internal table divs
-	d3.select(masterDivId).append("div").attr("class", "njhTableMenu");
+	//d3.select(masterDivId).append("div").attr("class", "njhTableMenu");
+	d3.select(masterDivId).append("div").attr("class", "njhTableMenuOrganized");
 	this.tabDiv = createTable(this.masterDivId);
 	d3.select(masterDivId).append("div").attr("class", "njhTableChart");
 	//populate table 
 	updateTable(this.tabDiv,this.tableMasterData["tab"],this.tableMasterData["columnNames"]);
-	var menu = d3.select(this.masterDivId + " .njhTableMenu");
-	this.menu = new njhCheckboxMenu(this.masterDivId + " .njhTableMenu", this.tableMasterData["columnNames"],this.updateTableOnClick.bind(this) );
-
+	//var menu = d3.select(this.masterDivId + " .njhTableMenu");
+	//this.menu = new njhCheckboxMenu(this.masterDivId + " .njhTableMenu", this.tableMasterData["columnNames"],this.updateTableOnClick.bind(this) );
+	
+	var menuOrganized = d3.select(this.masterDivId + " .njhTableMenuOrganized");
+	this.menuOrganized = new njhCheckboxMenuOrganized(this.masterDivId + " .njhTableMenuOrganized", this.tableMasterData["columnNames"],this.updateTableOnClickOrganized.bind(this) );
+	
 	var self = this;
 	//add download button for table 
-	menu.append("br");
+	menuOrganized.append("br");
+	menuOrganized.append("button").text("Download Table").attr("class", "njhTabSaveButton");
+	menuOrganized.select(".njhTabSaveButton").append("a").attr("class", "njhTabDownLink");
+	menuOrganized.select(".njhTabSaveButton").on("click", function() {
+		var allVals = [];
+		menuOrganized.selectAll("input:checked").each(function() {
+			allVals.push($(this).val());
+		});
+		var currentColumnNames = _.intersection(self.tableMasterData["columnNames"], allVals);
+		var mainTable = [];
+		mainTable.push(currentColumnNames);
+		//
+		for ( i = 0; i < self.tableMasterData["tab"].length; i++) {
+			var currentRow = [];
+			for (colNum in currentColumnNames) {
+				currentRow.push(self.tableMasterData["tab"][i][currentColumnNames[colNum]]);
+			}
+			mainTable.push(currentRow);
+		}
+		var dataSrc = 'data:text/csv;base64,' + btoa(d3.tsv.format(mainTable));
+		var downLink = menuOrganized.select(".njhTabDownLink");
+		downLink.attr("download", self.tableDownloadStubName + ".tab.csv");
+		downLink.attr("href", dataSrc);
+		downLink.node().click();
+	});
+	/*menu.append("br");
 	menu.append("button").text("Download Table").attr("class", "njhTabSaveButton");
 	menu.select(".njhTabSaveButton").append("a").attr("class", "njhTabDownLink");
 	menu.select(".njhTabSaveButton").on("click", function() {
@@ -39,7 +68,7 @@ function njhTable(masterDivId, tableMasterData, tableDownloadStubName, addChart)
 		downLink.attr("download", self.tableDownloadStubName + ".tab.csv");
 		downLink.attr("href", dataSrc);
 		downLink.node().click();
-	}); 
+	}); */
 	//create chart of numeric columns if needed
 	this.chart;
 	if (addChart) {
@@ -47,9 +76,18 @@ function njhTable(masterDivId, tableMasterData, tableDownloadStubName, addChart)
 	}
 }
 
-njhTable.prototype.updateTableOnClick = function() {
+/*njhTable.prototype.updateTableOnClick = function() {
 	var allVals = [];
 	d3.selectAll(this.masterDivId + " .njhTableMenu input:checked").each(function() {
+		allVals.push($(this).val());
+	});
+	var currentColumnNames = _.intersection(this.tableMasterData["columnNames"], allVals);
+	updateTable(this.tabDiv, this.tableMasterData["tab"], currentColumnNames);
+}; */
+
+njhTable.prototype.updateTableOnClickOrganized = function() {
+	var allVals = [];
+	d3.selectAll(this.masterDivId + " .njhTableMenuOrganized input:checked").each(function() {
 		allVals.push($(this).val());
 	});
 	var currentColumnNames = _.intersection(this.tableMasterData["columnNames"], allVals);
@@ -89,7 +127,8 @@ njhTable.prototype.updateWithData = function(updatedDataTab){
 	//as long as there isn't different column names this should work, other wise the table menu has to change
 	/**@todo might want to add in the ability to update with new column names*/
 	this.tableMasterData = updatedDataTab;
-	this.updateTableOnClick();
+	//this.updateTableOnClick();
+	this.updateTableOnClickOrganized();
 	if(this.addedChart){
 		this.addChart();
 	}
