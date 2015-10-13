08072145eda149bf1681df5c37cd78b274eb0ea0
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-10-13 12:25:13 -0400

    setting up current versions;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "SeekDeep"
-    libs = "bibseq:develop,njhRInside:develop,seqServer:develop"
+    libs = "bibseq:release/2.2.0,njhRInside:release/1.1,seqServer:release/1.2"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
