c679ba9bbda85591a9d44dfec15dcca8746baccc
Nicholas Hathaway <nickjhathaway@gmail.com>
2017-01-25 14:15:15 -0500

    no message

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "SeekDeep"
-    libs = "seqServer:v1.5.0"
+    libs = "seqServer:develop"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
