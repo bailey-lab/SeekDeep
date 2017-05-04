84a0c7cec8acd33bfcf5d5e9c960370165b643ff
Nicholas Hathaway <nickjhathaway@gmail.com>
2017-05-03 22:27:33 -0400

    for flash, must use relative path;

--- src/SeekDeepPrograms/SeekDeepUtils/SeekDeepUtilsRunner.cpp
+++ src/SeekDeepPrograms/SeekDeepUtils/SeekDeepUtilsRunner.cpp
@@ -598,8 +598,7 @@ int SeekDeepUtilsRunner::setupTarAmpAnalysis(
 						zcatR1 = bib::replaceString(zcatR1, "{OUTPUT}", bib::files::make_path(analysisSetup.dir_, key).string());
 						auto zcatR2 = bib::replaceString(zcatTempCmdR2, "{FILES}", bib::conToStr(readsByPairs.at(key).second, " "));
 						zcatR2 = bib::replaceString(zcatR2, "{OUTPUT}", bib::files::make_path(analysisSetup.dir_, key).string());
-						auto curStitchCmd = bib::replaceString(stitchCmd, "{OUTPUT}", bib::files::make_path(analysisSetup.dir_.relative_path(), key).string());
-
+						auto curStitchCmd = bib::replaceString(stitchCmd, "{OUTPUT}", bib::files::make_path(analysisSetup.dir_.lexically_relative( bfs::current_path()), key).string());
 						auto zcatR1Out = bib::sys::run( {zcatR1});
 						auto zcatR2Out = bib::sys::run( {zcatR2});
 						if(std::numeric_limits<uint32_t>::max() != analysisSetup.pars_.r1Trim) {
@@ -619,6 +618,7 @@ int SeekDeepUtilsRunner::setupTarAmpAnalysis(
 							bfs::remove(finalFnp);
 							bfs::rename(tempFnp, finalFnp);
 						}
+
 
 						if(std::numeric_limits<uint32_t>::max() != analysisSetup.pars_.r2Trim) {
 							auto finalFnp = bib::files::make_path(analysisSetup.dir_, key).string() + "_R2.fastq";
