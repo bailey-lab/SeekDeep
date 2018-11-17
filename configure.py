e256c1cb79c42f04e79b7a762aab4da255e6fcf1
Nicholas Hathaway <nickjhathaway@gmail.com>
2018-11-17 02:06:48 -0500

    bump version;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "SeekDeep"
-    libs = "seqServer:develop"
+    libs = "seqServer:v1.6.0"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
