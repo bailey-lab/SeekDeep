f34346c17655ee630bec112774d5c231cc578d31
Nicholas Hathaway <nickjhathaway@gmail.com>
2018-11-18 07:08:42 -0500

    Update to setup

--- scripts/setUpScripts/genFuncs.py
+++ scripts/setUpScripts/genFuncs.py
@@ -15,7 +15,7 @@ class genHelper:
                           "TWOBIT", "SEQSERVER","NJHRINSIDE", "PSTREAMS",
                            "MONGOC", "MONGOCXX", "SHAREDMUTEX",
                            "MAGIC", "HTS", "RESTBED", "LIBPCA", "BOOST_FILESYSTEM", 
-                           "ELUCIDATOR", "ELUCIDATOR-LAB", "EIGEN", "ZLIB", "ZLIB-NG"]
+                           "ELUCIDATOR", "ELUCIDATORLAB", "MIPWRANGLER", "EIGEN", "ZLIB", "ZLIB-NG"]
         neededLibraries = {}
         for lib in neededLibs:
             if ":" in lib:
