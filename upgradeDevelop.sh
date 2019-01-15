5883b1bc2889100040235a3c62f842cbc6db49c7
Nick Hathaway <nickjhathaway@gmail.com>
2019-01-15 17:44:22 -0500

    changed upgrade scripts;

--- upgradeDevelop.sh
+++ upgradeDevelop.sh
@@ -6,9 +6,12 @@ if [ $# -ne 1 ]; then
 fi
 
 #make sure on the master branch and then pull for any new commits
-git checkout master
+git checkout develop
 git pull
 #re-run install
 ./configure.py
+rm -fr external/local/njhseq/ external/local/seqServer/ external/local/TwoBit/ external/local/njhcpp/
+
 ./setup.py --compfile compfile.mk --outMakefile makefile-common.mk --overWrite
+
 make -j $1
