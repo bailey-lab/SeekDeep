37638784b3c28813a2d1e3676f2563436edd4cff
Nick Hathaway <nickjhathaway@gmail.com>
2019-07-08 21:55:17 -0400

    debug;

--- src/SeekDeep/objects/IlluminaNameFormatDecoder.cpp
+++ src/SeekDeep/objects/IlluminaNameFormatDecoder.cpp
@@ -11,7 +11,8 @@
 #include "IlluminaNameFormatDecoder.hpp"
 
 namespace njhseq {
-std::regex IlluminaNameFormatDecoder::NameRegPatStr_{""};
+//std::regex IlluminaNameFormatDecoder::NameRegPatStr_{""};
+std::regex IlluminaNameFormatDecoder::NameRegPatStr_{"([A-Za-z0-9_]+)"};
 //std::regex IlluminaNameFormatDecoder::NameRegPatStr_{"([A-Za-z0-9_]+):([0-9]+):([A-Za-z0-9-]+):([0-9]+):([0-9]+):([0-9]+):([0-9]+) ([12]):([NY]):([0-9]):([AGCTN+]+))"};
 
 }  // namespace njhseq
