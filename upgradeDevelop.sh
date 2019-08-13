7aa4956894c5f131899b8984d05eb67d1a8f368b
Nick Hathaway <nickjhathaway@gmail.com>
2019-08-13 15:37:21 -0700

    update to upgradeDevelop.sh

--- upgradeDevelop.sh
+++ upgradeDevelop.sh
@@ -10,6 +10,8 @@ git checkout develop
 git pull
 #re-run install
 ./configure.py
+rm -fr external/build/bamtools/ external/build/jsoncpp/ external/build/restbed/ 
+rm -fr external/local/bamtools/ external/local/jsoncpp/ external/local/restbed/ 
 rm -fr external/local/njhseq/ external/local/seqServer/ external/local/TwoBit/ external/local/njhcpp/
 
 ./setup.py --compfile compfile.mk --outMakefile makefile-common.mk --overWrite
